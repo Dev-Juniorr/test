9067c6fb613b14a80acffe87ca259c7c1d998d29
Joab Giraldo <joabgiraldo@gmail.com>
2022-09-06 15:58:48 -0500

    Connection MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+const mongoose = require("mongoose");
+const env_mongo = process.env.MONGO_URL;
+
+mongoose.connect(env_mongo);
+
+const database = mongoose.connection;
+database.on("error", (e) => {
+  console.log(e);
+});
+database.once("connected", () => {
+  console.log("Conectado");
+});
 
 const app = express();
 
@@ -11,4 +24,12 @@ app.get("/test", (req, res) => {
     message: "Hola mundo!",
   });
 });
+
+app.post("/test", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Envia un mensaje!",
+  });
+});
+
 app.listen(port, () => console.log(`Perfect Port ${port}`));
