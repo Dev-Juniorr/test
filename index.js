a3784745b83f89f695fc060513e54615ce691ea3
Joab Giraldo <joabgiraldo@gmail.com>
2022-09-06 14:44:01 -0500

    Third Commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-console.log('Hola mundo');
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Hola mundo!",
+  });
+});
+app.listen(port, () => console.log(`Perfect Port ${port}`));
